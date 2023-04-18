bb2f2e49cced95f6175416d13b27ca36797e7f8f
vrushantkathiriya <vrushantkathiriya@gmail.com>
2023-04-18 15:11:29 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "./API";  
+import { all } from "axios";
 
-function App() {
+const App = () => {
+  const [myData, setMyData] = useState([]);
+  const [isError, setIsError] = useState("");
+
+ 
+  const getMyPostData = async () => {
+    try {
+      const res = await axios.get("/posts");
+  
+      console.log(res.data);
+    } catch (error) {
+   
+      console.log(error.message);
+    }
+  };
+
+ 
+  useEffect(() => {
+    getMyPostData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1>blog</h1>
+      {isError !== "" && <h2>{isError}</h2>}
+
+      <div className="grid">
+        {myData.slice(all).map((post) => {
+          const { body, id, title } = post;
+          return (
+            <div key={id} className="card">
+              <h2>{title.slice(0, 15).toUpperCase()}</h2>
+              <p>{body.slice(0, 100)}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
